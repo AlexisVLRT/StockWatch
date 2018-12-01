9610641f78372eaf54457eb93e3f765c762ac329
Ugdha <alexisvlrt@gmail.com>
2018-12-01 16:30:31 +0100

    Sim tweaks

--- RealTimeStrat2.py
+++ RealTimeStrat2.py
@@ -14,7 +14,7 @@ import time
 
 
 class RealTime:
-    def __init__(self, db_id, n_cores, tickers, offset, queue):
+    def __init__(self, db_id, n_cores, tickers, cash, cash_per_trade,  offset, queue):
         self.db_id = db_id
         self.n_cores = n_cores
         self.tickers = tickers
@@ -24,8 +24,8 @@ class RealTime:
         self.order_threshold = 1
         self.sell_trigger_long = 'zero crossing'  # 'zero crossing' or 'extremum'
         self.sell_trigger_short = 'zero crossing'
-        self.cash = 5000000 / n_cores
-        self.start_cash = 85000
+        self.cash = cash / n_cores
+        self.cash_per_trade = cash_per_trade
         self.min_days_before_abort = 5
 
         self.offset = offset
@@ -37,8 +37,8 @@ class RealTime:
         self.future_data = self.future_data[self.tickers]
 
         self.positions, self.invested = DataBase.get_positions(self.db_id, self.tickers)
-        self.positions.loc[:, 'Invested'][self.positions['Invested'] == 0] = self.start_cash
-        self.positions.loc[:, 'Provisioned'][self.positions['Provisioned'] == 0] = self.start_cash
+        self.positions.loc[:, 'Invested'][self.positions['Invested'] == 0] = self.cash_per_trade
+        self.positions.loc[:, 'Provisioned'][self.positions['Provisioned'] == 0] = self.cash_per_trade
         self.positions['LongID'] = np.nan
         self.positions['ShortID'] = np.nan
         self.positions['LongDiff'] = np.nan
@@ -64,20 +64,16 @@ class RealTime:
                 signal = macd.ewm(span=9 * 390 // self.period).mean()
                 diff = (macd - signal)
                 diff = pd.Series(StandardScaler(with_mean=False).fit_transform(diff.values.reshape(-1, 1)).flatten())
-
-                macd_ = ema12 - ema24
-                signal_ = macd_.ewm(span=6 * 390 // self.period).mean()
-                diff_ = (macd_ - signal_)
-                diff_ = pd.Series(StandardScaler(with_mean=False).fit_transform(diff_.values.reshape(-1, 1)).flatten())
-                diff2 = (diff_.fillna(0).diff().ewm(span=0.5 * 390 // self.period).mean()).fillna(0) * 200
+                diff2 = (diff.fillna(0).diff().ewm(span=0.5 * 390 // self.period).mean()).fillna(0) * 200
 
                 # Buy when diff < -1 and diff2 goes > 0
-                if self.positions.loc[ticker, 'LongPosition'] == 0 and self.cash > self.start_cash:
-                    if diff.iloc[-1] < -1 and diff2.iloc[-1] > 0 > diff2.iloc[-2]:
-                        self.invested += self.start_cash
-                        self.cash -= self.start_cash
+                # Buy when diff > 0 and diff2 goes > 0
+                if self.positions.loc[ticker, 'LongPosition'] == 0 and self.cash > self.cash_per_trade:
+                    if diff.iloc[-1] > 0 and diff2.iloc[-1] > 0 > diff2.iloc[-2]:
+                        self.invested += self.cash_per_trade
+                        self.cash -= self.cash_per_trade
                         print('Buying ', ticker, i)
-                        self.open_position(ticker, entry_date=data.index[-1], position=1, entry_price=data.iloc[-1, 0], entry_money=self.start_cash, diff=diff.iloc[-1])
+                        self.open_position(ticker, entry_date=data.index[-1], position=1, entry_price=data.iloc[-1, 0], entry_money=self.cash_per_trade, diff=diff.iloc[-1])
 
                 # Sell when (diff > 0 and diff2 < 1) or (diff2 < 0 and diff < diff@buy/2 and j+3) or we achieve a sufficient year-based profit
                 if self.positions.loc[ticker, 'LongPosition']:
@@ -85,24 +81,25 @@ class RealTime:
                     min_date_abort = int(datetime.strftime(datetime.strptime(str(open_date), '%Y%m%d%H%M') + timedelta(days=self.min_days_before_abort), '%Y%m%d%H%M'))
 
                     price_in = self.positions.loc[ticker, 'LongPosition']
-                    y_profit = 0.5
+                    y_profit = 1
                     ticks = len(list(data.index)[data.index.get_loc(int(open_date)):])
                     thresh = y_profit * self.period * price_in / (390 * 250) * ticks + price_in
                     min_date_cashout = int(datetime.strftime(datetime.strptime(str(open_date), '%Y%m%d%H%M') + timedelta(hours=4), '%Y%m%d%H%M'))
-                    if (diff.iloc[-1] > 0 and diff2.iloc[-1] < 1) or (diff2.iloc[-1] < 0 and diff.iloc[-1] < self.positions.loc[ticker, 'LongDiff']*2/3 and int(list(data.index)[-1]) > min_date_abort) or (data.iloc[-1, 0] > thresh and int(list(data.index)[-1]) > min_date_cashout):
+                    # if (diff.iloc[-1] > 0 and diff2.iloc[-1] < 1) or (diff2.iloc[-1] < 0 and diff.iloc[-1] < self.positions.loc[ticker, 'LongDiff']*2/3 and int(list(data.index)[-1]) > min_date_abort) or (data.iloc[-1, 0] > thresh and int(list(data.index)[-1]) > min_date_cashout):
+                    if diff2.iloc[-1] < -0.5 or (data.iloc[-1, 0] > thresh and int(list(data.index)[-1]) > min_date_cashout):
                         new_money = data.iloc[-1, 0] / self.positions.loc[ticker, 'LongPosition'] * self.positions.loc[ticker, 'Invested']
                         self.invested -= self.positions.loc[ticker, 'Invested']
                         self.cash += new_money
                         print('Selling ', ticker, i)
                         self.close_position(ticker, exit_date=data.index[-1], position=1, exit_price=data.iloc[-1, 0], exit_money=new_money)
 
-                # Short when diff > 1 and diff2 < 0
-                if self.positions.loc[ticker, 'ShortPosition'] == 0 and self.cash > self.start_cash:
-                    if diff.iloc[-1] > 1 and diff2.iloc[-1] < 0 < diff2.iloc[-2]:
-                        self.invested += self.start_cash
-                        self.cash -= self.start_cash
+                # Short when diff > 1 and diff2 goes below 0
+                if self.positions.loc[ticker, 'ShortPosition'] == 0 and self.cash > self.cash_per_trade:
+                    if diff.iloc[-1] < 0 and diff2.iloc[-1] < 0 < diff2.iloc[-2]:
+                        self.invested += self.cash_per_trade
+                        self.cash -= self.cash_per_trade
                         print('Shorting ', ticker, i)
-                        self.open_position(ticker, entry_date=data.index[-1], position=-1, entry_price=data.iloc[-1, 0], entry_money=self.start_cash, diff=diff.iloc[-1])
+                        self.open_position(ticker, entry_date=data.index[-1], position=-1, entry_price=data.iloc[-1, 0], entry_money=self.cash_per_trade, diff=diff.iloc[-1])
 
                 # Cover when (diff < 0 and diff2 > -1) or (diff2 > 0 and diff > diff@buy/2 and j+3) or we achieve a sufficient year-based profit
                 if self.positions.loc[ticker, 'ShortPosition']:
@@ -110,12 +107,13 @@ class RealTime:
                     min_date_abort = int(datetime.strftime(datetime.strptime(str(open_date), '%Y%m%d%H%M') + timedelta(days=self.min_days_before_abort), '%Y%m%d%H%M'))
 
                     price_in = self.positions.loc[ticker, 'ShortPosition']
-                    y_profit = 0.5
+                    y_profit = 1
                     ticks = len(list(data.index)[data.index.get_loc(int(open_date)):])
                     thresh = -y_profit * self.period * price_in / (390 * 250) * ticks + price_in
                     min_date_cashout = int(datetime.strftime(datetime.strptime(str(open_date), '%Y%m%d%H%M') + timedelta(hours=4), '%Y%m%d%H%M'))
 
-                    if (diff.iloc[-1] < 0 and diff2.iloc[-1] > -1) or (diff2.iloc[-1] > 0 and diff.iloc[-1] > self.positions.loc[ticker, 'ShortDiff']*2/3 and int(list(data.index)[-1]) > min_date_abort) or (data.iloc[-1, 0] < thresh and int(list(data.index)[-1]) > min_date_cashout):
+                    # if (diff.iloc[-1] < 0 and diff2.iloc[-1] > -1) or (diff2.iloc[-1] > 0 and diff.iloc[-1] > self.positions.loc[ticker, 'ShortDiff']*2/3 and int(list(data.index)[-1]) > min_date_abort) or (data.iloc[-1, 0] < thresh and int(list(data.index)[-1]) > min_date_cashout):
+                    if diff2.iloc[-1] > 0.5 or (data.iloc[-1, 0] < thresh and int(list(data.index)[-1]) > min_date_cashout):
                         new_money = self.positions.loc[ticker, 'Provisioned'] / self.positions.loc[ticker, 'ShortPosition'] * (self.positions.loc[ticker, 'ShortPosition'] - data.iloc[-1, 0]) + self.positions.loc[ticker, 'Provisioned']
                         self.invested -= self.positions.loc[ticker, 'Provisioned']
                         self.cash += new_money
@@ -165,9 +163,11 @@ class RealTime:
         Thread(target=DataBase.close_position, args=(self.db_id, trade_id, exit_date, exit_price, exit_money, profit)).start()
 
 
-db_id = 1
+db_id = 2
 n_cores = 3
 n_stocks = 30
+cash = 5000000
+cash_per_trade = 500000
 offset = 10000
 past_data = pickle.load(open("FullData_5.p", "rb"))[:-offset]
 past_data = past_data[~past_data.index.duplicated(keep='last')]
@@ -184,7 +184,7 @@ if __name__ == '__main__':
     m = Manager()
     q = m.Queue()
     for i in range(n_cores):
-        workers = pool.apply_async(RealTime, (db_id, n_cores, tickers[i], offset, q))
+        workers = pool.apply_async(RealTime, (db_id, n_cores, tickers[i], cash, cash_per_trade, offset, q))
 
     last_point = time.time()
     plot = False
