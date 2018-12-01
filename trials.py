b82cad3c422d88e9b39f6484c4eb50255ea855bc
avialaret <alexisvlrt@gmail.com>
2018-12-01 09:47:00 +0100

    Is this a working strategy ?

--- trials.py
+++ trials.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 import seaborn;seaborn.set()
@@ -8,8 +9,8 @@ import os
 from iexfinance import Stock
 
 
-stock = 'CARB'
-data_path = 'D:/PickledStocksData'
+stock = 'FOLD'
+data_path = 'PickledStocksData'
 period = 5
 
 data = pd.read_csv('recap.csv', header=-1, index_col=1)
@@ -26,28 +27,31 @@ refined['MoneyDelta'] = refined['MoneyOut'] - refined['MoneyIn']
 
 
 hist_data = pd.DataFrame(pickle.load(open(data_path + "/{}_5.p".format(stock), "rb"))['open'])
-hist_data = hist_data[~hist_data.index.duplicated(keep='last')].iloc[:]
+hist_data = hist_data[~hist_data.index.duplicated(keep='last')].iloc[10946:]
 hist_data['Order'] = pd.Series()
 
-print(hist_data['open'].std())
 
-
-ema24 = hist_data.iloc[:, 0].ewm(span=26 * 390 // period).mean()
-ema12 = hist_data.iloc[:, 0].ewm(span=12 * 390 // period).mean()
+ema24 = hist_data.iloc[:, 0].ewm(span=26 * 390 // period * 0.5).mean()
+ema12 = hist_data.iloc[:, 0].ewm(span=12 * 390 // period * 0.5).mean()
 macd = ema12 - ema24
-signal = macd.ewm(span=9 * 390 // period).mean()
+signal = macd.ewm(span=9 * 390 // period * 0.5).mean()
 diff = (macd - signal)
 diff = pd.Series(StandardScaler(with_mean=False).fit_transform(diff.values.reshape(-1, 1)).flatten())
-diff2 = (diff.fillna(0).diff().ewm(span=0.5 * 390 // period).mean()).fillna(0)*200
-# diff2 = pd.Series(StandardScaler(with_mean=False).fit_transform(diff2.values.reshape(-1, 1)).flatten())
+diff_ = (diff.fillna(0).diff().ewm(span=0.5 * 390 // period).mean()).fillna(0) * 200
 
-ema24_ = hist_data.iloc[:, 0].ewm(span=26*4//5 * 390 // period).mean()
-ema12_ = hist_data.iloc[:, 0].ewm(span=12*4//5 * 390 // period).mean()
-macd_ = ema12_ - ema24_
-signal_ = macd_.ewm(span=9*4//5 * 390 // period).mean()
-diff_ = (macd_ - signal_)
-diff_ = pd.Series(StandardScaler(with_mean=False).fit_transform(diff_.values.reshape(-1, 1)).flatten())
-diff2_ = (diff_.fillna(0).diff().ewm(span=0.5 * 390 // period).mean()).fillna(0)*200
+ema242 = hist_data.iloc[:, 0].ewm(span=26 * 390 // period * 2).mean()
+ema122 = hist_data.iloc[:, 0].ewm(span=12 * 390 // period * 2).mean()
+macd2 = ema122 - ema242
+signal2 = macd2.ewm(span=9 * 390 // period * 2).mean()
+diff2 = (macd2 - signal2)
+diff2 = pd.Series(StandardScaler(with_mean=False).fit_transform(diff2.values.reshape(-1, 1)).flatten())
+
+ema243 = hist_data.iloc[:, 0].ewm(span=26 * 390 // period).mean()
+ema123 = hist_data.iloc[:, 0].ewm(span=12 * 390 // period).mean()
+macd3 = ema123 - ema243
+signal3 = macd3.ewm(span=9 * 390 // period).mean()
+diff3 = (macd3 - signal3)
+diff3 = pd.Series(StandardScaler(with_mean=False).fit_transform(diff3.values.reshape(-1, 1)).flatten())
 
 for ticker, (entry_date, exit_date, position, money_in, money_out, money_delta) in refined.iterrows():
     if position == 1:
@@ -61,11 +65,16 @@ for ticker, (entry_date, exit_date, position, money_in, money_out, money_delta) 
 
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex='all')
 
-ax2.fill_between(list(diff.reset_index(drop=True).index), diff.fillna(0).values, alpha=0.5)
-# ax2.fill_between(list(diff.reset_index(drop=True).index), diff2.fillna(0).values, alpha=0.5)
-# ax2.fill_between(list(diff.reset_index(drop=True).index), diff_.fillna(0).values, alpha=0.5)
-# ax2.fill_between(list(diff.reset_index(drop=True).index), diff2_.fillna(0).values, alpha=0.5)
+ax2.fill_between(list(diff2.reset_index(drop=True).index), (diff2 + diff3).fillna(0).values, alpha=0.5)
+ax2.fill_between(list(diff.reset_index(drop=True).index), diff_.fillna(0).values, alpha=0.5)
+# ax2.fill_between(list(diff.reset_index(drop=True).index), diff3.fillna(0).values, alpha=0.5)
 hist_data['open'].reset_index(drop=True).plot(ax=ax1)
+
+price_in = hist_data.iloc[0, 0]
+y_profit = 0.5
+a = -y_profit * period * price_in / (390 * 250)
+x = np.array(list(diff2.reset_index(drop=True).index))
+ax1.plot(a*x + price_in)
 
 for order in hist_data.dropna().iterrows():
     date, value = order[0], int(order[1]['Order'])
